35f5ad7c417b2de0a5a9c6f3dabcd4565fb33406
GannaKov <zlatta2000@gmail.com>
2024-06-13 20:14:14 +0200

    before I break

--- components/ui/CardHoverEffect.tsx
+++ components/ui/CardHoverEffect.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { cn } from "@/utils/cn";
@@ -35,7 +36,7 @@ export const HoverEffect = ({
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-purple-100 dark:bg-slate-800/[0.8] block  rounded-3xl"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
@@ -69,10 +70,11 @@ export const Card = ({
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black-100 border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-[#13162D] dark:bg-black-100  border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
         className
       )}
     >
+      <img src="/bg.png" alt="bgimg" className="absolute top-0 left-0" />
       <div className="relative z-50">
         <div className="p-4">{children}</div>
       </div>
@@ -87,7 +89,7 @@ export const CardTitle = ({
   children: React.ReactNode;
 }) => {
   return (
-    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+    <h4 className={cn("text-white font-bold tracking-wide mt-4", className)}>
       {children}
     </h4>
   );
@@ -102,7 +104,7 @@ export const CardDescription = ({
   return (
     <p
       className={cn(
-        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+        "mt-8 text-white-100 tracking-wide leading-relaxed text-sm",
         className
       )}
     >
