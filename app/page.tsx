34a333707a77267877e1031d480358c04966d60e
GannaKov <zlatta2000@gmail.com>
2025-01-28 16:48:53 +0100

    clean

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ const Home = (): JSX.Element => {
   return (
     <div className="relative bg-black-100 flex justify-center items-center flex-col mx-auto overflow-clip">
       <div className=" w-full">
-        {/* mask bg-grid-purple-200/[0.1]*/}
-        <div className="h-full w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-purple-200/[0.1]  absolute top-0 left-0 flex items-center justify-center">
+        {/* maskdark: bg-grid-white/[0.05]*/}
+        <div className="h-full w-full dark:bg-black-100 bg-white   bg-grid-purple-200/[0.1]  absolute top-0 left-0 flex items-center justify-center">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] " />
         </div>
         {/* end mask */}
